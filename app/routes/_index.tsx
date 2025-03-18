e3b869cbf957f36f45e916a8a02963be0171d857
mhabbatein <habiburrahmanmuhammad95@gmail.com>
2025-03-19 02:01:28 +0700

    second commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -30,7 +30,7 @@ export default function Index() {
         </header>
         <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
           <p className="leading-6 text-gray-700 dark:text-gray-200">
-            What&apos;s next?
+          Apa Selanjutnya?
           </p>
           <ul>
             {resources.map(({ href, text, icon }) => (
